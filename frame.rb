05c9f3a13cbdaba89a37b5f65a6b926165cce776
Moisés Ñañez <mnanez@able.co>
2020-02-05 11:16:03 -0500

    Add screenshot

--- frame.rb
+++ frame.rb
@@ -1,27 +1,25 @@
 require "watir"
 
 b = Watir::Browser.new :firefox
-b.goto "https://codesandbox.io/s/formik-example-o26nd"
 
-b.iframe(id: "sandbox-preview").button(text: "Submit").click
-b.iframe(id: "sandbox-preview").text_field(id: "email").set "Demo"
-b.iframe(id: "sandbox-preview").div(class: "input-feedback").exists?
-b.iframe(id: "sandbox-preview").text_field(id: "email").set "Demo@able.co"
-b.iframe(id: "sandbox-preview").button(text: "Submit").click
+# b.goto "https://codesandbox.io/s/formik-example-o26nd"
+# b.iframe(id: "sandbox-preview").button(text: "Submit").click
+# b.iframe(id: "sandbox-preview").text_field(id: "email").set "Demo"
+# b.iframe(id: "sandbox-preview").div(class: "input-feedback").exists?
+# b.iframe(id: "sandbox-preview").text_field(id: "email").set "Demo@able.co"
+# b.iframe(id: "sandbox-preview").button(text: "Submit").click
 
-# b = Watir::Browser.new :firefox
 # b.goto "https://o26nd.csb.app/"
-
-# b.button(text: "Submit").wait_until_present.click
+# b.button(text: "Submit").wait_until(&:present?).click
 # b.text_field(id: "email").set "Demo"
 # b.div(class: "input-feedback").exists?
 # b.text_field(id: "email").set "Demo@able.co"
 # b.button(text: "Submit").click
+# b.screenshot.save 'screenshot.png'
 
 sleep 2
 
 b.alert.exists?
 b.alert.ok
 
-sleep 3
-
+sleep 1
