715fd7cdfd9678cce29a2c2d1ed3375a3f821f43
Moisés Ñañez <mnanez@able.co>
2020-02-05 10:34:10 -0500

    Add iframe example

--- intro.rb
+++ intro.rb
@@ -4,7 +4,7 @@ b = Watir::Browser.new :chrome, headless: true
 b.goto "https://translate.google.com/?hl=es#view=home&op=translate&sl=en&tl=es"
 textarea = b.textarea id: "source"
 textarea.set "Hello everyone! Today's techlunch is about Watir"
-listen = b.div(aria_label: "Escuchar")
+listen = b.div aria_label: "Escuchar"
 listen.click
 
 sleep 5
